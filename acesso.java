0eb920713b2b2722fb9a3305f3c0da04f41c91f1
JONY DIAS <jony.dias70@gmail.com>
2023-09-07 10:56:33 -0400

    Update acesso.java

--- acesso.java
+++ acesso.java
@@ -4,4 +4,5 @@ public class acesso {
   execution (void acesso.validaemail(String email));
   execution (void acesso.validaCPFCNPJ(String cpfcnpj));
 	//...segue o codigo
+  execution (void Pessoa.log(String email, String cpfcnpj));
 }
